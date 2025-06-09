edf55100f6f85e1d35a2e124eb520736fc2d172a
NicoMKTXS <nwang@marketaxess.com>
2025-06-09 11:02:35 -0400

    Dummy Kafka + Avro Project Done.

--- src/main/java/avro/messaging/ProducerDemo.java
+++ src/main/java/avro/messaging/ProducerDemo.java
@@ -1,10 +1,8 @@
 package avro.messaging;
 
-import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,43 +23,19 @@ public class ProducerDemo {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        // create a Kafka producer
+        // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        for (int i=0; i<10; i++ ) {
-            // create a producer record
-            ProducerRecord<String, String> producerRecord =
-                    new ProducerRecord<String, String>("demo_java", "hello world " + Integer.toString(i));
+        // create a producer record
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demo_java", "hello world");
 
-            // send data - asynchronous
-            producer.send(producerRecord, new Callback() {
-                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                    // executes every time a record is successfully sent or an exception is thrown
-                    if (e == null) {
-                        // the record was successfully sent
-                        log.info("Received new metadata. \n" +
-                                "Topic:" + recordMetadata.topic() + "\n" +
-                                "Partition: " + recordMetadata.partition() + "\n" +
-                                "Offset: " + recordMetadata.offset() + "\n" +
-                                "Timestamp: " + recordMetadata.timestamp());
-                    } else {
-                        log.error("Error while producing", e);
-                    }
-                }
-            });
+        // send data - asynchronous
+        producer.send(producerRecord);
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        // send data - sync
+        // flush data - synchronous
         producer.flush();
-
         // flush and close producer
         producer.close();
-
     }
 }
